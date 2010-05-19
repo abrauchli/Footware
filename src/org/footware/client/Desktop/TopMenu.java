88f55a904a1b5686ffd4182e8fd4b199a52f84e7
flwidmer <flwidmer@localhost>
2010-05-19 15:02:41 +0200

    login box

--- src/org/footware/client/Desktop/TopMenu.java
+++ src/org/footware/client/Desktop/TopMenu.java
@@ -1,16 +1,13 @@
 package org.footware.client.Desktop;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import org.footware.client.dialogs.LoginBox;
+import org.footware.client.dialogs.UploadTrackBox;
 import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 
 public class TopMenu extends MenuBar {
-	private LoginBox lb = new LoginBox();
+	private LoginBox lb = new LoginBox(this);
 	private UploadTrackBox ub = new UploadTrackBox();
 	private MenuItem logout;
 	private MenuItem login;
@@ -55,33 +52,6 @@ public class TopMenu extends MenuBar {
 	public void logout() {
 		logout.setVisible(false);
 		login.setVisible(true);
-	}
-
-	private class LoginBox extends DialogBox {
-		public LoginBox() {
-			setText("Login");
-			Button close = new Button("close");
-			close.addClickHandler(new ClickHandler() {
-
-				@Override
-				public void onClick(ClickEvent event) {
-					lb.hide();
-				}
-			});
-			Button login = new Button("Login");
-			login.addClickHandler(new ClickHandler() {
-
-				@Override
-				public void onClick(ClickEvent event) {
-					login();
-					hide();
-				}
-			});
-			HorizontalPanel hp = new HorizontalPanel();
-			hp.add(login);
-			hp.add(close);
-			add(hp);
-		}
 	}
 
 }
