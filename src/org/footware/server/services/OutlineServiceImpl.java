0b6a431b5ea84974ef62e117141c7a5eb46f7180
flwidmer <flwidmer@localhost>
2010-05-27 04:56:24 +0200

    comments work

--- src/org/footware/server/services/OutlineServiceImpl.java
+++ src/org/footware/server/services/OutlineServiceImpl.java
@@ -71,54 +71,33 @@ public class OutlineServiceImpl extends RemoteServiceServlet implements
 
 	@Override
 	public List<TrackDTO> getTrackList(TrackSearchData filter) {
-//		List<TrackDTO> children = new ArrayList<TrackDTO>();
-//		// List<Track> tracks = TrackUtil.getAllPublicTracks();
-//		List<Track> tracks = new ArrayList<Track>();
-//		tracks.add(new Track(new User(new UserDTO()), "f", "ff"));
-//		for (Track t : tracks) {
-//			children.add(t.getTrackDTO());
-//		}
-//		return children;
-		return getDemoTracks();
+		List<TrackDTO> children = new ArrayList<TrackDTO>();
+		List<Track> tracks = TrackUtil.getAllPublicTracks();
+		tracks.add(new Track(new User(new UserDTO()), "f", "ff"));
+		for (Track t : tracks) {
+			children.add(t.getTrackDTO());
+		}
+		return children;
 	}
 
 	public String[][] getTracksTable(TrackSearchData filter) {
-		// List<Track> tracks = TrackUtil.getAllPublicTracks();
-//		List<Track> tracks = new ArrayList<Track>();
-//		tracks.add(new Track(new User(new UserDTO()), "f", "ff"));
-//		String[][] result = new String[tracks.size()][6];
-//		for (int i = 0; i < tracks.size(); i++) {
-//			result[i][0] = tracks.get(i).getUser().getFullName();
-//			result[i][1] = Integer.toString(tracks.get(i).getTrackpoints());
-//			result[i][2] = Double.toString(tracks.get(i).getLength());
-//			result[i][3] = tracks.get(i).getStartTime().toString();
-//			result[i][4] = Integer.toString(tracks.get(i).getComments().size());
-//			StringBuffer b = new StringBuffer();
-//			for (Tag tag : tracks.get(i).getTags()) {
-//				b.append(tag.getTag() + ", ");
-//			}
-//			result[i][5] = b.toString();
-//
-//		}
-//		System.out.println("This is the 5000th line of code :)");
-//		return result;
-		String[][] result = new String[][]{{"Florian Widmer","5","2000.0","12.1.2010","0"}};
-		return result;
-	}
+		List<Track> tracks = TrackUtil.getAllPublicTracks();
+		tracks.add(new Track(new User(new UserDTO()), "f", "ff"));
+		String[][] result = new String[tracks.size()][6];
+		for (int i = 0; i < tracks.size(); i++) {
+			result[i][0] = tracks.get(i).getUser().getFullName();
+			result[i][1] = Integer.toString(tracks.get(i).getTrackpoints());
+			result[i][2] = Double.toString(tracks.get(i).getLength());
+			result[i][3] = tracks.get(i).getStartTime().toString();
+			result[i][4] = Integer.toString(tracks.get(i).getComments().size());
+			StringBuffer b = new StringBuffer();
+			for (Tag tag : tracks.get(i).getTags()) {
+				b.append(tag.getTag() + ", ");
+			}
+			result[i][5] = b.toString();
 
-	private List<TrackDTO> getDemoTracks() {
-		List<TrackDTO> result = new ArrayList<TrackDTO>();
-		TrackDTO t = new TrackDTO();
-		t.setCommentsEnabled(true);
-		t.setLength(2000.0);
-		t.setFilename("aaa");
-		t.setPublicity(5);
-		UserDTO u = new UserDTO();
-		u.setEmail("t@t.c");
-		u.setFullName("tt");
-		t.setUser(u);
-		u.addTrackDTO(t);
-		result.add(t);
+		}
+		System.out.println("This is the 5000th line of code :)");
 		return result;
 	}
 
